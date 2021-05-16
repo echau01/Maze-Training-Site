ab08924d89c0ad1da0f06b2b286c4cd223b29008
Steven <steven.yan7888@gmail.com>
2021-05-15 19:29:48 -0700

    Improved memory efficiency by making the array-based queue shift left after >20 pop operations.

--- src/maze/queue.ts
+++ src/maze/queue.ts
@@ -5,6 +5,7 @@ export class Queue<T> {
     private entry: number;
     private exit: number;
     private _size: number;
+    private _pops: number;
 
     // Constructor for an array-based implementation of a queue.
     constructor() {
@@ -12,6 +13,7 @@ export class Queue<T> {
         this.entry = 0;
         this.exit = 0;
         this._size = 0;
+        this._pops = 0;
     }
 
     // Pushes an item into the queue.
@@ -27,15 +29,33 @@ export class Queue<T> {
         delete this._arr[this.exit];
 
         // Checks if the queue has been exhausted. Resets the entry and exit indexes if so.
+        // Also resets the number of pop operations to prevent early shifting after resets.
         if (this.entry == this.exit && this.entry != 0) {
             this.entry = 0;
             this.exit = 0;
+            this._pops = 0;
         } else {
             this.exit++;
         }
-
         this._size--;
+
+        // Shift the array over after >20 pop operations to get rid of empty space at the start.
+        if (this._pops++ > 20) {
+            this.shift();
+            this._pops = 0;
+        }
         return ret;
+    }
+
+    // Removes blank space at the start of the array holding the queue.
+    shift() {
+        let shifted_arr = [];
+        for (let i = 0; i < this._size; i++) {
+            shifted_arr[i] = this._arr[i + this.exit];
+        }
+        this._arr = shifted_arr;
+        this.entry = this._size;
+        this.exit = 0;
     }
 
     // Returns the size of the queue.
